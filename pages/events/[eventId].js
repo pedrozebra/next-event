aa41e5d3703baa505e32d76c09a0749c00ababc5
Pietro Della Notte <pdellanotte@alpenite.com>
2021-04-13 12:16:17 +0200

    Modified detail event page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,7 +1,31 @@
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent  from "../../components/event-detail/EventContent";
+
 export default function EventDetailPage() {
-    return (
-        <div>
-            <h1>Event Detail Page</h1>
-        </div>
-    )
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event || !event.isFeatured) {
+    return <p>No event found</p>;
+  }
+
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
 }
